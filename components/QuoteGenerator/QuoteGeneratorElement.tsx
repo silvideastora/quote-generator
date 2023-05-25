8ed1d9ff650e6e2afd26c1d0abee98cfbdc8bb3f
Silvia rodriguez <silvideastora18@gmail.com>
2023-05-25 17:39:04 -0600

    feature quote generator element

---
+++ components/QuoteGenerator/QuoteGeneratorElement.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+
+export const GradientBackgroundCon = styled.div`
+  background: linear-gradient(to right, #000046, #9d23ee);
+  background-size: 400% 400%;
+  animation: gradient 6s ease infinite;
+  width: 100vw;
+  height: 100vh;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElement'
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Quote Generator</title>
+        <meta name="description" content="A project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/*Background*/ }
+      <GradientBackgroundCon>
+        ggggg
+      </GradientBackgroundCon>
+    </>
+  )
+}
